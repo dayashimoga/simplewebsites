ad7d2bba818e9fed4cd03cd20ceb312a9501734c
dayashimoga <dayashimoga@gmail.com>
2026-04-03 22:03:19 +0530

    chore: add terraform import blocks to repair cloudflare pages project state out-of-sync

---
+++ terraform/import.tf
@@ -0,0 +1,9 @@
+import {
+  to = cloudflare_pages_project.sites["geology-earth-lab"]
+  id = "geology-earth-lab"
+}
+
+import {
+  to = cloudflare_pages_project.sites["electricity-magnetism-lab"]
+  id = "electricity-magnetism-lab"
+}
